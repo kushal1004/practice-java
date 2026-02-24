4376589caf9adad2b34748b2d7d2879abb1d4019
Kushal <kushalsingha00@gmail.com>
2026-02-24 08:29:40 +0530

    chore: comments add

--- Constructor.java
+++ Constructor.java
@@ -33,3 +33,7 @@ public class Constructor {
         new Car().show(); // anonymous object
     }
 }
+
+
+// two questions : can i used super() method to call any thing from parents class or only just constructor 
+// Another question is when i used anonymous object means just new A.show(), each and every time when i used new A thats' means it will create new heap memory ??
