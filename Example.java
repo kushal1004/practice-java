6145039402baa46fdb5ab8fb4386f1da33108bf7
Kushal <kushalsingha00@gmail.com>
2026-04-01 00:34:02 +0530

    chore : eod

--- Example.java
+++ Example.java
@@ -1,5 +1,14 @@
-public class Example{
-    public static void main(String[] args){
-        
+public class Example {
+    public static void main(String[] args) {
+        String str = "Java";
+        str.concat("world");
+        System.out.println(str);
+        String str1 = new String("Java");
+        if (str == str1) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+        System.out.println(str.equals(str1));
     }
 }
